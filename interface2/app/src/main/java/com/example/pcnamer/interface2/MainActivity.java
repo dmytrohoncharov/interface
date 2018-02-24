300d4761b18ce0d8f370ce73358b93071b7b7520
dmytrohoncharov <dmitriihoncharov@gmail.com>
2018-02-24 13:51:49 +0100

    debug test

--- interface2/app/src/main/java/com/example/pcnamer/interface2/MainActivity.java
+++ interface2/app/src/main/java/com/example/pcnamer/interface2/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.example.pcnamer.interface2.actions.ActionBuildContainer;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context ctx = getApplicationContext();
+        ActionBuildContainer test = new ActionBuildContainer(1);
         //Vo_login log = new Vo_login(ctx);
         //Log.e("tag", log.setXml());
 
